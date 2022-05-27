fccab16100098cc266c0a2885c6b585dda3a94f5
Padmaja2022 <99272460+Padmaja2022@users.noreply.github.com>
2022-05-27 10:31:13 -0400

    test commit

---
+++ BankApp/src/test/java/com/example/BankApp/BankAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BankApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
