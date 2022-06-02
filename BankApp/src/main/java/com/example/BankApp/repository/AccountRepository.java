3e85ea32cce8574d710e602df8b310df0b0370bc
BijayWagle <wbijay5@gmail.com>
2022-06-02 10:58:11 -0700

    updated

--- BankApp/src/main/java/com/example/BankApp/repository/AccountRepository.java
+++ BankApp/src/main/java/com/example/BankApp/repository/AccountRepository.java
@@ -2,8 +2,10 @@ package com.example.BankApp.repository;
 
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.BankApp.model.Accounts;
+import org.springframework.stereotype.Repository;
 
+import com.example.BankApp.model.Accounts;
+@Repository
 public interface AccountRepository extends JpaRepository<Accounts, Long> {
 
 }
--- BankApp/src/main/java/com/example/BankApp/service/AccountService.java
+++ BankApp/src/main/java/com/example/BankApp/service/AccountService.java
@@ -1,11 +1,12 @@
 package com.example.BankApp.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.example.BankApp.model.Accounts;
 import com.example.BankApp.repository.AccountRepository;
 
-
+@Service
 public class AccountService extends Accounts {
 
 @Autowired
