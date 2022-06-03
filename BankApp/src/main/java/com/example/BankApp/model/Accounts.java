d83c6064b4a088b3dea985cbea5ea48453df0299
BijayWagle <wbijay5@gmail.com>
2022-06-03 14:52:54 -0700

    added foreign key constraint

--- BankApp/src/main/java/com/example/BankApp/model/Accounts.java
+++ BankApp/src/main/java/com/example/BankApp/model/Accounts.java
@@ -3,12 +3,17 @@ package com.example.BankApp.model;
 
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import lombok.AccessLevel;
@@ -92,5 +97,9 @@ public class Accounts {
 		return lastTwoOfSSN + uniqueID + randomNumber;
 
 	}
+	//one to many foreign key constraint
+	@OneToMany(cascade=CascadeType.ALL)
+	@JoinColumn(name="trsid",referencedColumnName="accid")
+	List<Transactions>transactions=new ArrayList<>();
 
 }
