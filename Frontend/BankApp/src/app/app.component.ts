965d4725c098ef1e64c6d28bfa7302641fe87d68
dlnhnr <30333417+dlnhnr@users.noreply.github.com>
2022-06-03 16:40:37 -0400

    removed forms

--- Frontend/BankApp/src/app/app.component.ts
+++ Frontend/BankApp/src/app/app.component.ts
@@ -7,33 +7,4 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'BankApp';
-  showModal!: boolean;
-  registerForm!: FormGroup;
-  submitted = false;
-  constructor(private formBuilder: FormBuilder) {}
-  show()
-  {
-    this.showModal = true;
-  }
-  hide()
-  {
-    this.showModal = false;
-  }
-  ngOnInit() {
-    this.registerForm = this.formBuilder.group({
-      email: ['', [Validators.required, Validators.email]],
-      password: ['',[Validators.required, Validators.minLength(6)]]
-    });
-  }
-  get f() {return this.registerForm.controls; }
-  onSubmit() {
-    this.submitted = true;
-    if(this.registerForm.invalid) {
-      return;
-    }
-    if(this.submitted) {
-      this.showModal = false;
-    }
-  }
 }
