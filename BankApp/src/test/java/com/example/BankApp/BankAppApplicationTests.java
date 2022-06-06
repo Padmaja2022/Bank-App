c54d5eeb8bd460fa56f84ca40fe4dfecbd5f60c5
TangoTuesday <99271990+TangoTuesday@users.noreply.github.com>
2022-06-06 15:47:53 -0500

    junit tests
    wrote a few junit test cases

--- BankApp/src/test/java/com/example/BankApp/BankAppApplicationTests.java
+++ BankApp/src/test/java/com/example/BankApp/BankAppApplicationTests.java
@@ -3,11 +3,31 @@ package com.example.BankApp;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.example.BankApp.service.AccountService;
+import com.example.BankApp.service.TransactionsService;
 @SpringBootTest
 class BankAppApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
-
+	
+	@Test 
+	void makeDeposit() {
+		
+		var makeDeposit = new TransactionsService();
+		makeDeposit.deposit(1L, 123) ;
+		
+	}
+	@Test 
+	void getBalance() {
+		var getBalance = new TransactionsService();
+		getBalance.Balance(1L);
+		
+	}
+	@Test 
+	void makeWidthDraw() {
+		var makeWidthDraw = new TransactionsService();
+		makeWidthDraw.widthDraw(1L, 100); 
+	}
 }
--- Frontend/BankApp/src/app/components/profile/profile.component.ts
+++ Frontend/BankApp/src/app/components/profile/profile.component.ts
@@ -13,3 +13,4 @@ export class ProfileComponent implements OnInit {
   }
 
 }
+
