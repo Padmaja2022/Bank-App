373d48ba670847ec29e866ce8558767394995746
TangoTuesday <99271990+TangoTuesday@users.noreply.github.com>
2022-06-07 11:34:59 -0500

    Update BankAppApplicationTests.java
    created more junit tests for accountService class

--- BankApp/src/test/java/com/example/BankApp/BankAppApplicationTests.java
+++ BankApp/src/test/java/com/example/BankApp/BankAppApplicationTests.java
@@ -30,4 +30,44 @@ class BankAppApplicationTests {
 		var makeWidthDraw = new TransactionsService();
 		makeWidthDraw.widthDraw(1L, 100); 
 	}
+	
+/*	@Test 
+	void asdfs() {
+		var asdf = new TransactionsService();
+		asdf.
+	}
+*/	
+	@Test
+	void getAccount() {
+		var getAccount = new AccountService(); 
+		getAccount.getAccount(1L);
+		
+	}
+	@Test
+	void updatePhoneNumber() {
+		
+		var updatePhoneNumber = new AccountService();
+		updatePhoneNumber.updatePhone(1L, "5555555555");
+		
+		
+	}
+	
+	@Test
+	void updateEmail() { 
+		var updateEmail = new AccountService();
+		updateEmail.updateEmail(1L, "yes@gmail.com");
+	}
+	
+	@Test
+	void updateFirstName() {
+		var updateFirstName = new AccountService() ;
+		updateFirstName.updateFirstName(1L, "Dylan");
+	}
+	
+	@Test
+	void updateLastName() {
+		var updateLastName = new AccountService();
+		updateLastName.updateLastName(1L, "Henry");
+	}
+		
 }
