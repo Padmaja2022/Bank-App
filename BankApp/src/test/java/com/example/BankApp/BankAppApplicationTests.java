fe238b1befcd7634df7d14cf7ec97c35b3f6ef02
TangoTuesday <99271990+TangoTuesday@users.noreply.github.com>
2022-06-08 10:57:35 -0500

    mhm
    mhmh

--- BankApp/src/test/java/com/example/BankApp/BankAppApplicationTests.java
+++ BankApp/src/test/java/com/example/BankApp/BankAppApplicationTests.java
@@ -1,9 +1,11 @@
 package com.example.BankApp;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.example.BankApp.model.Accounts;
 import com.example.BankApp.repository.AccountRepository;
 import com.example.BankApp.repository.TransactionsRepository;
 import com.example.BankApp.service.AccountService;
@@ -11,70 +13,79 @@ import com.example.BankApp.service.TransactionsService;
 
 @SpringBootTest
 class BankAppApplicationTests {
-@Autowired
-	private AccountRepository aaa;
-	private TransactionsRepository bbb; 
-
+/*	@Mock
+	AccountRepository aaa;
+	@Mock
+	TransactionsRepository bbb; 
+*/
+	@Autowired
+	TransactionsService abc = new TransactionsService(); 
+	@Autowired 
+	AccountService bcd = new AccountService();
+	
 	@Test
 	void contextLoads() {
 	}
 	
 	@Test 
-	void makeDeposit() {
+void makeDeposit() {
 		
-		var makeDeposit = new TransactionsService();
-		makeDeposit.deposit(1L, 123) ;
-		
-	}
-	@Test 
-	void getBalance() {
-		var getBalance = new TransactionsService();
-		getBalance.Balance(1L);
+		//var makeDeposit = new TransactionsService();
+	abc.deposit(15L, 123.0);
 		
 	}
+	//@Test 
+	//void getBalance() {
+	//	var getBalance = new TransactionsService();
+	//	abc.Balance(1L);
+		
+	//}
+	
+	
+	
 	@Test 
 	void makeWidthDraw() {
-		var makeWidthDraw = new TransactionsService();
-		makeWidthDraw.widthDraw(1L, 100); 
+	//	var makeWidthDraw = new TransactionsService();
+		abc.widthDraw(15L, 100); 
+	} 
+	
+	@Test 
+	void initDeposit() {
+		bcd.initDeposit(1L, 1000);
+		
 	}
 	
-/*	@Test 
-	void asdfs() {
-		var asdf = new TransactionsService();
-		asdf.
-	}
-*/	
 	@Test
 	void getAccount() {
-		var getAccount = new AccountService(); 
-		getAccount.getAccount(1L);
+//		var getAccount = new AccountService(); 
+		bcd.getAccount(1L);
 		
 	}
 	@Test
 	void updatePhoneNumber() {
 		
-		var updatePhoneNumber = new AccountService();
-		updatePhoneNumber.updatePhone(1L, "5555555555");
+	//	var updatePhoneNumber = new AccountService();
+		bcd.updatePhone(1L, "5555555555");
 		
 		
 	}
 	
 	@Test
 	void updateEmail() { 
-		var updateEmail = new AccountService();
-		updateEmail.updateEmail(1L, "yes@gmail.com");
+	//	var updateEmail = new AccountService();
+		bcd.updateEmail(1L, "yes@gmail.com");
 	}
 	
 	@Test
 	void updateFirstName() {
-		var updateFirstName = new AccountService() ;
-		updateFirstName.updateFirstName(1L, "Dylan");
+	//	var updateFirstName = new AccountService() ;
+		bcd.updateFirstName(1L, "Dylan");
 	}
 	
 	@Test
 	void updateLastName() {
-		var updateLastName = new AccountService();
-		updateLastName.updateLastName(1L, "Henry");
+		//var updateLastName = new AccountService();
+		bcd.updateLastName(1L, "Henry");
 	}
 		
 }
